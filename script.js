6eb8c910dc03950b32ab3f87faeb306f0b3784a4
szglabs <sdrummond95@gmail.com>
2020-12-23 17:26:29 -0500

    working for +,-,/,*

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
 'use strict'
+
+let displayValue = '0';
+let oldValue = '';
+let operator = '';
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
+
+// updating the display
+const updateDisplay = function() {
+    if (displayValue.length < 9) {
+        display.textContent = displayValue;
+    } else if (displayValue.length >= 9) {
+        display.textContent = displayValue.substring(0,9)
+    }
+}
+
+updateDisplay();
+
+for (let i = 0; i <buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+        if (buttons[i].classList.contains('digit')) {
+            if (displayValue === '0') {
+                displayValue = buttons[i].value;
+                updateDisplay();
+            } else if (operator != '') {
+                displayValue += buttons[i].value;
+                updateDisplay();
+            } else {
+                displayValue += buttons[i].value;
+                updateDisplay();
+            }
+        } else if (buttons[i].classList.contains('ac')) {
+            displayValue = '0';
+            oldValue = '';
+            operator = '';
+            updateDisplay();
+        } else if (buttons[i].classList.contains('operator')) {
+            oldValue = displayValue;
+            operator = buttons[i].value;
+            displayValue = '';
+        } else if (buttons[i].classList.contains('equals') && oldValue != '' && operator != '') {
+            displayValue = `${operate(parseInt(oldValue), parseInt(displayValue), operator)}`;
+            updateDisplay();
+            console.log(displayValue)
+        }
+    })
+}
+
+
+
+// Operations
+const operate = (a, b, op) => {
+    if (op === '+') {
+        return a + b;
+    } else if (op === '-') {
+        return a - b;
+    } else if (op === '*') {
+        return a * b;
+    } else if (op === '/') {
+        return a / b;
+    }
+}
